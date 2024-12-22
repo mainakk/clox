ddfc1691750d45f54d364f4d8ab18dc0066e97c2
Mainak Kundu <mainak1729@gmail.com>
2024-12-21 22:57:24 -0500

    Scanning on Demand

--- debug.c
+++ debug.c
@@ -10,7 +10,7 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
-static int simpleInstruction(const const char* name, int offset) {
+static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
